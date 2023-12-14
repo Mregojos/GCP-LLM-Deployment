a381687357a3e40f451cab66fd45e68ea320fb9f
Matt <mattiregojos@gmail.com>
2023-12-14 02:48:46 +0000

    Add and modify files

---
+++ update.sh
@@ -0,0 +1,6 @@
+# Model Deployement Repo
+cp -rf git-push.sh ~/GCP*Deployment/git-push.sh
+cp -rf env*  ~/GCP*Deployment/env*
+cp -rf app-deployment.sh  ~/GCP*Deployment/infra*
+cp -rf cleanup.sh  ~/GCP*Deployment/appcleanup
+cp -rf app-deployment/* ~/GCP*Deployment/app
