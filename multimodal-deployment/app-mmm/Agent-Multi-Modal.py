b0df4c41105b5eed0db30c28663e7d66dd47be1d
Matt <mattiregojos@gmail.com>
2023-12-16 15:45:13 +0000

    Add and modify files

--- multimodal-deployment/app-mmm/Agent-Multi-Modal.py
+++ multimodal-deployment/app-mmm/Agent-Multi-Modal.py
@@ -523,9 +523,11 @@ def version_ii(con, cur):
     with st.sidebar:
         default_name = "Matt"
         input_name = st.text_input("Name", default_name)
-        model = st.selectbox("Choose Chat Model, Vision Model or Multi-Modal Model", ("Chat Model", "Vision Model", "Multi-Modal Model"))
+        model = st.selectbox("Choose Chat Model, Vision Model or Multi-Modal Model", ("Chat Model", "Vision Model", "Vision with DB Model"))
         prompt_user = st.text_area("Prompt")
         if model == "Vision Model":
+            if prompt_user == "":
+                prompt_user = "What is the image? Tell me more about the image."   
             uploaded_file = st.file_uploader("Upload a photo", type=["jpg", "jpeg", "png"])
             if uploaded_file is not None:
                 image_data = uploaded_file.read()
@@ -533,10 +535,10 @@ def version_ii(con, cur):
                 st.image(image_data, image_name)
                 image_data_base = base64.b64encode(image_data)
                 # st.write(image_data_base)
-                image = Part.from_data(data=base64.b64decode(image_data_base), mime_type="image/png")
-                if prompt_user == "":
-                    prompt_user = "What is the image? Tell me more about the image."    
-        if model == "Multi-Modal Model":
+                image = Part.from_data(data=base64.b64decode(image_data_base), mime_type="image/png")  
+        if model == "Vision with DB Model":
+            if prompt_user == "":
+                prompt_user = "What is the image? Tell me more about the image."  
             uploaded_file = st.file_uploader("Upload a photo", type=["jpg", "jpeg", "png"])
             if uploaded_file is not None:
                 image_data = uploaded_file.read()
@@ -547,8 +549,7 @@ def version_ii(con, cur):
                 # image_data_base_string_data = base64.b64decode(image_data_base_string)
                 # st.image(image_data_base_string_data)
                 image = Part.from_data(data=base64.b64decode(image_data_base), mime_type="image/png")
-                if prompt_user == "":
-                    prompt_user = "What is the image? Tell me more about the image."    
+  
         current_time = t.strftime("Date: %Y-%m-%d | Time: %H:%M:%S UTC")
         prompt_history = "You are an intelligent Agent."
         button = st.button("Send")
@@ -588,12 +589,15 @@ def version_ii(con, cur):
                 con.commit()
             
             elif model == "Vision Model":
-                start_time = t.time() 
-                current_model = "Multi-Modal Model"
-                responses = multimodal_model.generate_content([prompt_user, image], generation_config=multimodal_generation_config)
-                end_time = t.time()
+                if uploaded_file is None:
+                    st.info("Upload file first")
+                else:
+                    start_time = t.time() 
+                    current_model = "Multi-Modal Model"
+                    responses = multimodal_model.generate_content([prompt_user, image], generation_config=multimodal_generation_config)
+                    end_time = t.time()
 
-            elif model == "Multi-Modal Model":
+            elif model == "Vision with DB Model":
                 start_time = t.time() 
                 current_model = "Multi-Modal Model"
                 cur.execute(f"""
@@ -602,18 +606,33 @@ def version_ii(con, cur):
                         WHERE name='{input_name}'
                         ORDER BY time ASC
                         """)
-                # for id, name, prompt, output, model, time, start_time, end_time, saved_image_data, image_data_base in cur.fetchall():
-                #     saved_image = Part.from_data(data=base64.b64decode(image_data_base), mime_type="image/png")
-                #    responses = multimodal_model.generate_content([prompt, saved_image], generation_config=multimodal_generation_config)
-                responses = multimodal_model.generate_content([prompt_user, image], generation_config=multimodal_generation_config)
-                output = responses.text
-                end_time = t.time()
-
-                ### Insert into a table
-                SQL = "INSERT INTO chats_multimodal (name, prompt, output, model, time, start_time, end_time, saved_image_data_base_string) VALUES(%s, %s, %s, %s, %s, %s, %s, %s);"
-                data = (input_name, prompt_user, output, current_model, current_time, start_time, end_time, image_data_base_string)
-                cur.execute(SQL, data)
-                con.commit()
+                for id, name, prompt, output, model, time, start_time, end_time, saved_image_data_base_string in cur.fetchall():
+                    if saved_image_data_base_string is not None:
+                        image_data_base_string_data = base64.b64decode(saved_image_data_base_string)
+                        image_data_base = base64.b64encode(image_data_base_string_data)
+                        saved_image = Part.from_data(data=base64.b64decode(image_data_base), mime_type="image/png")       
+                        responses = multimodal_model.generate_content([prompt, saved_image], generation_config=multimodal_generation_config)
+                    else:
+                        responses = multimodal_model.generate_content(prompt, generation_config=multimodal_generation_config)
+                if uploaded_file is not None:
+                    responses = multimodal_model.generate_content([prompt_user, image], generation_config=multimodal_generation_config)
+                    end_time = t.time()
+                    output = responses.text
+                    ### Insert into a table
+                    SQL = "INSERT INTO chats_multimodal (name, prompt, output, model, time, start_time, end_time, saved_image_data_base_string) VALUES(%s, %s, %s, %s, %s, %s, %s, %s);"
+                    data = (input_name, prompt_user, output, current_model, current_time, start_time, end_time, image_data_base_string)
+                    cur.execute(SQL, data)
+                    con.commit()
+                else:
+                    responses = multimodal_model.generate_content(prompt_user, generation_config=multimodal_generation_config)
+                    end_time = t.time()
+                    output = responses.text
+                    ### Insert into a table
+                    SQL = "INSERT INTO chats_multimodal (name, prompt, output, model, time, start_time, end_time) VALUES(%s, %s, %s, %s, %s, %s, %s);"
+                    data = (input_name, prompt_user, output, current_model, current_time, start_time, end_time)
+                    cur.execute(SQL, data)
+                    con.commit()
+                
                 
         prune = st.button(":red[Prune History]")
         if prune:
@@ -652,12 +671,15 @@ def version_ii(con, cur):
             message.caption(f"{time} | Model: {model} | Processing Time: {round(end_time-start_time, round_number)} seconds")
     
     elif model == "Vision Model" and button:
-        message = st.chat_message("assistant")
-        message.image(image_data)
-        message.markdown(responses.text)
-        message.caption(f"{current_time} | Model: {current_model} | Processing Time: {round(end_time-start_time, round_number)} seconds")
+        if uploaded_file is None:
+            pass
+        else:
+            message = st.chat_message("assistant")
+            message.image(image_data)
+            message.markdown(responses.text)
+            message.caption(f"{current_time} | Model: {current_model} | Processing Time: {round(end_time-start_time, round_number)} seconds")
 
-    elif model == "Multi-Modal Model" and button:
+    elif model == "Vision with DB Model":
         cur.execute(f"""
         SELECT * 
         FROM chats_multimodal
@@ -666,8 +688,9 @@ def version_ii(con, cur):
         """)
         for id, name, prompt, output, model, time, start_time, end_time, saved_image_data_base_string in cur.fetchall():
             message = st.chat_message("assistant")
-            image_data_base_string_data = base64.b64decode(saved_image_data_base_string)
-            message.image(image_data_base_string_data)
+            if saved_image_data_base_string is not None:
+                image_data_base_string_data = base64.b64decode(saved_image_data_base_string)
+                message.image(image_data_base_string_data)
             message.markdown(output)
             message.caption(f"{time} | Model: {model} | Processing Time: {round(end_time-start_time, round_number)} seconds")
 
