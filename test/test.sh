98a9a9c4ab4e2774c225ecde228745122e834a1a
Matt <mattiregojos@gmail.com>
2023-12-27 09:21:06 +0000

    Add and modify files

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,10 @@
-echo "Run some tests..."
+echo "Run some tests... \n"
 
 # Check if the database is reachable
+# psycopg2
+echo "pip install psycopg2-binary -q"
+pip install psycopg2-binary -q
+echo "Finished: psycopg2-binary \n"
 
 # Check if the Cloud Run Endpoint is ready
 
