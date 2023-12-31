0acf88845aeeb3450e6b936fc405b33d98635a93
Matt <mattiregojos@gmail.com>
2023-12-31 11:27:52 +0000

    Add and modify files

--- app-dev-api.sh
+++ app-dev-api.sh
@@ -1,4 +1,9 @@
 # Enable APIs
-gcloud 
+# gcloud services list --available | grep <service name>
+gcloud services enable compute.googleapis.com iam.googleapis.com cloudbuild.googleapis.com artifactregistry.googleapis.com run.googleapis.com aiplatform.googleapis.com cloudresourcemanager.googleapis.com
+# Generative Language API
+gcloud services enable generativelanguage.googleapis.com
+
+echo "\n #----------Services have been successfully enabled.----------# \n"
 
 # 
