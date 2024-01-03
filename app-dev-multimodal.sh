ab7141af4300b3f415638294a1992bfacbd790e4
Matt <mattiregojos@gmail.com>
2024-01-03 05:50:48 +0000

    Add and modify files

--- app-dev-multimodal.sh
+++ app-dev-multimodal.sh
@@ -1,4 +1,8 @@
 #----------Enable Artifact Registry, Cloud Build, and Cloud Run, Vertex AI----------#
+# gcloud services list --available
+gcloud services enable compute.googleapis.com iam.googleapis.com cloudbuild.googleapis.com artifactregistry.googleapis.com run.googleapis.com aiplatform.googleapis.com cloudresourcemanager.googleapis.com
+echo "\n #----------Services have been successfully enabled.----------# \n"
+
 # Directory
 cd app-dev-multimodal
 
