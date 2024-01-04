9971deaa3f7f7ae4e82e9b4b54bc59e0d3aea188
Matt <mattiregojos@gmail.com>
2024-01-04 10:33:48 +0000

    Add and modify files

--- app-dev-ii/Agent.py
+++ app-dev-ii/Agent.py
@@ -65,6 +65,7 @@ if __name__ == '__main__':
     # Connection
     con = False
     mm_model, mm_chat = models()
+    column_num = 2
 
     try:
         con, cur = connection()
@@ -72,52 +73,115 @@ if __name__ == '__main__':
     except:
         st.info("##### :computer: ```DATABASE CONNECTION: The app can't connect to the database right now. Please try again later.```")
     if con == True:
-        st.info("Database Connected")
+        # st.info("Database Connected")
         # Connection
         con, cur = connection()
         
-        info_sample_prompts = """
-            You can now start the conversation by prompting in the text bar. Enjoy. :smile: You can ask:
-            * What is Cloud Computing?
-            * What is Google Cloud?
-            * Important Google Cloud Services to know
-            * Compare Site Reliability Engineering with DevOps
-            * Tell me about different cloud services
-            * Explain Cloud Computing in simple terms
-            * Tell me a funny quote related to Cloud Computing
-            """
-        st.info(info_sample_prompts)
+        apps = st.selectbox("Tools", ["Text Only (One-Turn)", "Code Analysis (One-Turn)"])
         
-        prompt = st.text_area("Prompt")
-        button = st.button("Generate")
-        button_stream = st.button("Generate (Stream)")
-        # button_multi_turn = st.button("Generate (Multi-Turn)")
-        refresh = st.button("Refresh")
-        
-        start = t.time()
-        if prompt and button:
-            response = mm_chat.send_message(prompt)
-            # st.write(response.text)
-            st.markdown(response.text)
-            # st.text(mm_chat.history)
-            end = t.time()
-            # st.caption(f"Total Processing Time: {end - start}")
-        if prompt and button_stream:
-            response_ = ""
-            response = mm_chat.send_message(prompt, stream=True)
-            st.info("Output streaming... \n")
-            for chunk in response:
-                st.write(chunk.text)
-                response_ = response_ + chunk.text 
-            st.info("Output in markdown \n")
-            st.markdown(response_)
-            end = t.time()
-            # st.caption(f"Total Processing Time: {end - start}")
-        # if prompt and button_multi_turn:            
-        #    pass
-            # st.caption(f"Total Processing Time: {end - start}")
-        if refresh:
-            st.rerun()
-            
-        # for message in mm_chat.history:
-        #    st.markdown(f'**{message.role}**: {message.parts[0].text}')
+        if apps == "Text Only (One-Turn)":
+
+            info_sample_prompts = """
+                You can now start the conversation by prompting in the text bar. Enjoy. :smile: You can ask:
+                * What is Cloud Computing?
+                * What is Google Cloud?
+                * Important Google Cloud Services to know
+                * Compare Site Reliability Engineering with DevOps
+                * Tell me about different cloud services
+                * Explain Cloud Computing in simple terms
+                * Tell me a funny quote related to Cloud Computing
+                """
+            st.info(info_sample_prompts)
+
+            col_A, col_B = st.columns(column_num)
+
+            with col_A:
+
+                prompt = st.text_area("Prompt")
+                button = st.button("Generate")
+                button_stream = st.button("Generate (Stream)")
+                refresh = st.button("Refresh")
+                # button_multi_turn = st.button("Generate (Multi-Turn)")
+
+
+            with col_B:
+                start = t.time()
+                if prompt and button:
+                    response = mm_chat.send_message(prompt)
+                    # st.write(response.text)
+                    st.info("Output in markdown \n")
+                    st.markdown(response.text)
+                    # st.text(mm_chat.history)
+                    end = t.time()
+                    # st.caption(f"Total Processing Time: {end - start}")
+                if prompt and button_stream:
+                    response_ = ""
+                    response = mm_chat.send_message(prompt, stream=True)
+                    st.info("Output streaming... \n")
+                    for chunk in response:
+                        st.write(chunk.text)
+                        response_ = response_ + chunk.text 
+                    st.info("Output in markdown \n")
+                    st.markdown(response_)
+                    end = t.time()
+                    # st.caption(f"Total Processing Time: {end - start}")
+                # if prompt and button_multi_turn:            
+                #    pass
+                    # st.caption(f"Total Processing Time: {end - start}")
+                if refresh:
+                    st.rerun()
+
+                # for message in mm_chat.history:
+                #    st.markdown(f'**{message.role}**: {message.parts[0].text}')
+                
+        if apps == "Code Analysis (One-Turn)":
+
+            info_sample_prompts = """
+                You can now start the conversation by prompting in the text bar. Enjoy. :smile: You can ask:
+                * What is this code about?
+                * How to optimize this code?
+                """
+            st.info(info_sample_prompts)
+
+            col_A, col_B = st.columns(column_num)
+
+            with col_A:
+                
+                code_text = st.text_area("Code")
+                prompt = st.text_area("Prompt")
+                prompt = f"This is the code: \n\n {code_text} \n\n {prompt}"
+                button = st.button("Generate")
+                button_stream = st.button("Generate (Stream)")
+                refresh = st.button("Refresh")
+                # button_multi_turn = st.button("Generate (Multi-Turn)")
+
+
+            with col_B:
+                start = t.time()
+                if prompt and button:
+                    response = mm_chat.send_message(prompt)
+                    # st.write(response.text)
+                    st.info("Output in markdown \n")
+                    st.markdown(response.text)
+                    # st.text(mm_chat.history)
+                    end = t.time()
+                    # st.caption(f"Total Processing Time: {end - start}")
+                if prompt and button_stream:
+                    response_ = ""
+                    response = mm_chat.send_message(prompt, stream=True)
+                    st.info("Output streaming... \n")
+                    for chunk in response:
+                        st.write(chunk.text)
+                        response_ = response_ + chunk.text 
+                    st.info("Output in markdown \n")
+                    st.markdown(response_)
+                    end = t.time()
+                    # st.caption(f"Total Processing Time: {end - start}")
+                # if prompt and button_multi_turn:            
+                #    pass
+                    # st.caption(f"Total Processing Time: {end - start}")
+                if refresh:
+                    st.rerun()
+
+                # for message in mm_chat.history:
+                #    st.markdown(f'**{message.role}**: {message.parts[0].text}')
